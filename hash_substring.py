5117fbd034e38f64a26eb77bfe0675bf3c957c86
0wlie22 <103207663+0wlie22@users.noreply.github.com>
2023-05-12 12:59:04 +0300

    Edit hash function

--- hash_substring.py
+++ hash_substring.py
@@ -19,28 +19,40 @@ def print_occurrences(output: list):
     print(' '.join(map(str, output)))
 
 
-def hash_function(pattern: str):
+def hash_function(text: str, i: int, chash: int = 0, len_pattern: int = 0) -> int:
     # create a hash function
-    hpattern = 0
-    for i in range(len(pattern)):
-        hpattern += ord(pattern[i]) * 263 ** i
-
-    return hpattern
+    cur_hash = chash
+    if i > 0 and i <= len(text) - len_pattern + 1:
+        cur_hash -= ord(text[i - 1]) * 263 ** (len_pattern - 1)
+        cur_hash += ord(text[i + len_pattern - 1]) * 263 ** (len_pattern - 1)
+    elif (i == 0):
+        cur_hash = 0
+        for j in range(i + len_pattern):
+            cur_hash += ord(text[j]) * 263 ** j
+    elif (i == -1):
+        len_pattern = len(text)
+        cur_hash = 0
+        for j in range(len_pattern):
+            cur_hash += ord(text[j]) * 263 ** j
+    return cur_hash
 
 
 def get_occurrences(pattern: str, text: str):
-    # create an empty list to store the occurencies
-    occurencies = []
-    # create a hash function
-    hpattern = hash_function(pattern)
+    # create an empty list to store the occurences
+    occurrences = []
+    hpattern = hash_function(pattern, -1)
+    len_pattern = len(pattern)
 
     # iterate over the text and check if the hash of the pattern is equal to the hash of the text
+    cur_hash = 0
     for i in range(len(text) - len(pattern) + 1):
-        if hash_function(text[i:i + len(pattern)]) == hpattern:
-            occurencies.append(str(i))
+        tpattern = hash_function(text, i, cur_hash, len_pattern)
+        cur_hash = tpattern
+        if cur_hash == hpattern:
+        # if the hash values match, check character by character if the pattern matches the text
+            occurrences.append(str(i))
 
-    return occurencies
-
+    return occurrences
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
