0172bc6be8b0d7243f3cf4a489c2df240d11924b
0wlie22 <103207663+0wlie22@users.noreply.github.com>
2023-05-12 11:36:50 +0300

    Change hash function

--- hash_substring.py
+++ hash_substring.py
@@ -1,9 +1,11 @@
 def read_input():
     method: str = input()[0]
     match method:
+        # read from input
         case "I":
             pattern: str = input().rstrip()
             text: str = input().rstrip()
+        # read from file
         case "F":
             with open("tests/06", 'r') as file:
                 content: list = file.readlines()
@@ -17,12 +19,24 @@ def print_occurrences(output: list):
     print(' '.join(map(str, output)))
 
 
-def get_occurrences(pattern: str, text: str):
-    occurencies = []
-    hpattern = hash(pattern)
+def hash_function(pattern: str):
+    # create a hash function
+    hpattern = 0
+    for i in range(len(pattern)):
+        hpattern += ord(pattern[i]) * 263 ** i
 
+    return hpattern
+
+
+def get_occurrences(pattern: str, text: str):
+    # create an empty list to store the occurencies
+    occurencies = []
+    # create a hash function
+    hpattern = hash_function(pattern)
+
+    # iterate over the text and check if the hash of the pattern is equal to the hash of the text
     for i in range(len(text) - len(pattern) + 1):
-        if hash(text[i:i + len(pattern)]) == hpattern:
+        if hash_function(text[i:i + len(pattern)]) == hpattern:
             occurencies.append(str(i))
 
     return occurencies
